836f3d841b4c270e2545b4effbe8200813ed6a5b
Matthias Van Woensel <3532563+matthiaz@users.noreply.github.com>
2020-02-06 13:08:23 +0100

    adding --with-termlib

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -24,7 +24,8 @@ class Ncurses < Formula
                           "--enable-symlinks",
                           "--enable-widec",
                           "--with-shared",
-                          "--with-gpm=no"
+                          "--with-gpm=no",
+                          "--with-termlib"
     system "make", "install"
     make_libncurses_symlinks
 
